345ba5d4d087a50d4307790a51c31377197cf827
Patrick Shushereba <pshushereba@gmail.com>
2019-12-14 09:53:04 -0800

    Set up endpoints with JWT.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,55 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets.js');
+
+const Users = require('../users/users-model.js');
 
 router.post('/register', (req, res) => {
-  // implement registration
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+  .then((newUser) => {
+      const token = generateToken(newUser);
+      res.status(201).json({created_user: newUser, token: token})
+  })
+  .catch((err) => {
+      res.status(500).json(error)
+  });
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  const { username, password } = req.body;
+  
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ username: user.username, token: token });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
+
+function generateToken(user) {
+  const payload = {
+    userid: user.id,
+    username: user.username,
+    department: user.department   
+  };
+  const options = {
+      expiresIn: '1h'
+  }
+  const token = jwt.sign(payload, secrets.jwtSecret, options);
+
+  return token;
+}
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,25 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets.js');
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+    
+  const token = req.headers.authorization.split(' ')[1];
+  
+  if (req.decodedJwt) { // will check for a token that is already authorized.
+    next();
+  } else if (token && (token !== undefined)) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedJwt) => {
+      if (err) {
+        res.status(401).json({ you: 'shall not pass!' });
+      } else {
+        req.decodedJwt = decodedJwt;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ you: 'shall not pass!' });
+  }
 };
